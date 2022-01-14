af040dff8ccc182fca965ca4a196d2345120595c
Héctor Rubén Vázquez Osorio <Rubeenvz@outlook.com>
2022-01-14 17:12:21 -0600

    Table has been added, some components have been added, etc.
    Table has been added, some components have been added, etc.

--- src/components/pokemons/Table.jsx
+++ src/components/pokemons/Table.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react'
+
+import TableRow from './TableRow'
+
 import IconSearchGray from '../../img/icon_search-gray.svg'
-import IconShow from '../../img/icon_show.svg'
 import IconRight from '../../img/icon_right.svg'
 import IconLeft from '../../img/icon_left.svg'
 
 function Table() {
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/'
+    const response = await (await fetch(url)).json()
+    let pokemons = await Promise.all(response.results.map(async (element) => {
+      let pokemon = await (await fetch(element.url)).json()
+      return pokemon
+    }))
+    setPokemons(pokemons)
+    
+  },[])
 
   return (
     <>
@@ -24,35 +40,18 @@ function Table() {
         <div className="mt-6 overflow-scroll">
           <table className="w-full">
             <tr className="text-left">
-              <th className="hidden lg:table-cell"><input type="checkbox" /></th>
+              <th className="hidden lg:table-cell">ID</th>
               <th><p className="text-sm text-primary font-montserrat font-semibold">Pokemon</p></th>
-              <th><p className="text-sm text-primary font-montserrat font-semibold">Area</p></th>
               <th><p className="text-sm text-primary font-montserrat font-semibold">Type</p></th>
-              <th><p className="text-sm text-primary font-montserrat font-semibold">Base Experience</p></th>
+              <th><p className="text-sm text-primary font-montserrat font-semibold text-center">Weight</p></th>
+              <th><p className="text-sm text-primary font-montserrat font-semibold text-center">Height</p></th>
+              <th><p className="text-sm text-primary font-montserrat font-semibold text-center">Base</p></th>
               <th><p className="text-sm text-primary font-montserrat font-semibold text-center">Detail</p></th>
             </tr>
             <tbody>
-              <tr className="text-left">
-                <td className="hidden lg:table-cell"><input type="checkbox" /></td>
-                <td>
-                  <div className="flex gap-4 items-center">
-                    <div className="adm-user__img hidden lg:block"></div>
-                    <article className="text-sm text-primary font-open font-semibold">
-                      us.name us.last_name
-                    </article>
-                  </div>
-                </td>
-                <td><p className="text-sm text-primary font-open">area[us.area]</p></td>
-                <td><p className="text-sm text-primary font-open">us.email</p></td>
-                <td><p className="tag text-sm text-primary font-open" className="us.status" >status[us.status]</p></td>
-                <td>
-                  <div className="flex items-center justify-center gap-4 flex-col lg:flex-row">
-                    <article>
-                      <img alt="Icon" src={IconShow} />
-                    </article>
-                  </div>
-                </td>
-              </tr>
+            {pokemons.map((pokemon) => {
+              return (<TableRow key={pokemon.id} data={pokemon} />) 
+            })}
             </tbody>
           </table>
         </div>
