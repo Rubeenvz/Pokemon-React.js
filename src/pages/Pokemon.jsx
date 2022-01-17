c0e4a814e1f72cbaedccfc7ce00ae8b691e311bf
Héctor Rubén Vázquez Osorio <Rubeenvz@outlook.com>
2022-01-17 14:01:42 -0600

    Breadcrumb has been improved
    Breadcrumb has been improved

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+import Breadcrumb from '../components/layout/Breadcrumb'
 import Info from '../components/pokemon/Info'
 
 function Pokemon() {
-  
+
+  const navigate = useNavigate();
+
   return (
     <div>
+      <div>
+        <Breadcrumb title="Pokemon"/>
+      </div>
       <Info />
+      <button className="button-primary" onClick={e => {navigate(-1)}}>
+        Go back!
+      </button>
     </div>
   )
 }
--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -1,9 +1,13 @@
+import Breadcrumb from '../components/layout/Breadcrumb'
 import Table from '../components/pokemons/Table'
 
 function Pokemons() {
   
   return (
     <div>
+      <div>
+        <Breadcrumb title="Pokemons"/>
+      </div>
       <Table />
     </div>
   )
